6d59f80e331aa5b43cc3c2907d49716399111e30
Davidsd16 <81986262+Davidsd16@users.noreply.github.com>
2021-05-13 13:51:57 +0200

    intro mvc

--- src/index.php
+++ src/index.php
@@ -1,20 +1,8 @@
 <?php
 
-include_once 'connection.php';
+namespace App;
+
+use App\controllers\MeetingController;
 
 
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="meetingList.php">Meeting List</a>
-</body>
-</html>
+$controller = new MeetingController();
